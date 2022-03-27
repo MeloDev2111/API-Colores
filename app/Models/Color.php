a3487c30912e692835322f045ccf685f339a2e90
MeloDev <melodiaz2111@gmail.com>
2022-03-27 11:54:32 -0500

    model Color fillables columns established

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Color extends Model
 {
     use HasFactory;
+    // editable columns
+    protected $fillable = ["name", "color", "pantone_value", "year"];
 }
